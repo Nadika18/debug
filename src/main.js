2da9cc854b1c51aff4ed02b037a08ecfabf93b99
Nadika Poudel <nadikapoudel16@gmail.com>
2020-08-12 09:38:10 +0545

    First commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import Vue from "vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+Vue.component("alert", require("./components/Alert.vue").default);
 
 createApp(App)
   .use(router)
